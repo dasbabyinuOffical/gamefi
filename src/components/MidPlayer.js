29123cae7e683245d165236abdffaa74e52cc867
liucong <cong.liu@ascendex.io>
2022-06-18 19:43:40 +0800

    添加洗牌

--- src/components/MidPlayer.js
+++ src/components/MidPlayer.js
@@ -1,23 +1,56 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
+import xipai from "@/assets/music/xipai.mp3";
+import "@/components/animate.css";
 
 function MidPlayer() {
+  //初始牌
   const [pokers, setPokers] = useState([]);
-
   useEffect(() => {
     const pokerList = [];
     for (let i = 0; i < 54; i++) {
-      pokerList.push(i);
+      pokerList.push({
+        index: i,
+        class: "back",
+        top: "-" + i + "px",
+        animation: "",
+      });
     }
     setPokers(pokerList);
   }, []);
 
+  // 洗牌audio
+  const audioRef = useRef(null);
+
+  // 洗牌
+  function clearPoker() {
+    //把初始牌组数据的顺序打乱,想乱一些，i的值可以设置更大一些
+    // 洗牌音效
+    audioRef.current.play();
+
+    const pokerList = pokers.map((item, index) => {
+      if (index < 27) {
+        item.animation = "run-1-0 9s linear " + index / 10 + "s 1";
+      } else {
+        item.animation = "run-1-0 6s linear " + index / 10 + "s 1";
+      }
+      return item;
+    });
+    setPokers(pokerList);
+    setTimeout(() => {
+      audioRef.current.pause();
+    }, 12000);
+  }
+
   return (
     <div className="mid">
       <div className="mid_top">
-        <ul className="all_poker">
+        <ul className="all_poker" onClick={clearPoker}>
           {pokers.map((item) => (
-            <li className="back" key={item} style={{ top: `-${item}px` }}></li>
+            <li
+              className={item.class}
+              key={item.index}
+              style={{ top: item.top, animation: item.animation }}
+            ></li>
           ))}
         </ul>
       </div>
@@ -59,6 +92,9 @@ function MidPlayer() {
             <ul className="play_2"></ul> {/*手牌*/}
           </div>
         </div>
+      </div>
+      <div className="music1" style={{ position: "absolute" }}>
+        <audio ref={audioRef} src={xipai}></audio>
       </div>
     </div>
   );
